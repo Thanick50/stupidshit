c95be62b57f3e3a19273221c1bedaad39ba8435b
thanick49 <97436042+Thanick50@users.noreply.github.com>
2024-09-28 04:44:48 -0400

    Update test.js

--- test.js
+++ test.js
@@ -3,7 +3,17 @@
 <body>
 
 
-<script>alert(1);</script>
+<script>// Prompt user for input
+var userInput = prompt("Please enter some text:");
+
+// Check if input is not null
+if (userInput !== null) {
+    // Log the input to the console
+    console.log("User input: " + userInput);
+    // You can also display it using alert
+    alert("You entered: " + userInput);
+}
+</script>
 
 
 </body>
